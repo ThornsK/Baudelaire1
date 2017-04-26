ad3e14ef98bd5eba720fa010f267786a57f24ded
Etudiant <Etudiant>
2017-04-26 15:32:03 +0200

    Connection

--- profil.php
+++ profil.php
@@ -5,5 +5,5 @@ require_once("inc/init.inc.php");
 require_once("inc/head.php");
 ?>
 
-<h1>Ceci est un profil</h1>
+<h1>Ceci est le profil de <?php if(isset($_SESSION["membre"])){ echo $_SESSION["membre"]["pseudo"];} ?></h1>
 
